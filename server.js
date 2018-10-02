b09d653302a6d6dfc56741af21f884c199bd311f
Itamar Bar-Lev <i@itu.sh>
2018-10-02 20:44:36 +0300

    Replacing http with websockets

--- server.js
+++ server.js
@@ -1,75 +1,71 @@
-var app = require("express")();
-var bodyParser = require("body-parser");
-var http = require("http").Server(app);
+const WebSocket = require('ws');
 
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
+const wss = new WebSocket.Server({ port: port });
 
-app.use(bodyParser.urlencoded({ extended: true }));
 
-var maxId = 0;
-var magnets = {};
+let maxId = 0;
+let magnets = {};
 
 function reset() {
-  maxId = 0;
-  magnets = {};
-  var initialWords = ["nothing", "just", "fine", "fish", "mine", "sloth", "cat", "and", "again", "or", "and", "not", "are", "the", "the", "surely", "kitchen", "fabric", "no", "not"];
+    maxId = 0;
+    magnets = {};
+    let initialWords = [
+        "nothing", "just", "fine", "fish", "mine", "sloth",
+        "cat", "and", "again", "or", "and", "not", "are",
+        "the", "the", "surely", "kitchen", "fabric", "no",
+        "not"
+    ];
 
-  initialWords.forEach(function(word) {
-    maxId++;
-    magnets[maxId] = {
-      id: maxId.toString(),
-      word: word,
-      position: {
-        x: Math.floor(Math.random() * 1000),
-        y: Math.floor(Math.random() * 1000)
-      },
-      rotation: (Math.random() * 4) - 2
-    };
-  });
+    initialWords.forEach(function(word) {
+        maxId++;
+        magnets[maxId] = {
+            id: maxId.toString(),
+            word: word,
+            position: {
+                x: Math.floor(Math.random() * 1000),
+                y: Math.floor(Math.random() * 1000)
+            },
+            rotation: (Math.random() * 4) - 2
+        };
+    });
 }
 reset();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    next();
-});
+wss.on('connection', function connection(ws) {
+    ws.send(JSON.stringify(magnets));
+    ws.on('message', function incoming(data) {
+        // Validate the incoming json.
+        try {
+            var move = JSON.parse(data);
+        }
+        catch (e) {
+            console.log('invalid json: ' + data);
+            return;
+        }
+        if (isNaN(move.i) || isNaN(move.x) || isNaN(move.y) || isNaN(move.r)) {
+            console.log('invalid data: ' + data);
+            return;
+        }
+        if (Object.keys(move).length !== 4) {
+            console.log('too many properties: ' + data);
+            return;
+        }
+        if (!magnets[move.i]) {
+            console.log('no such id: ' + data);
+            return;
+        }
 
-app.get("/reset", function(req, res) {
-  reset();
-  res.send(magnets);
-});
+        // Update the move on the magnet.
+        magnets[move.i].rotation = move.r;
+        magnets[move.i].position.x = move.x;
+        magnets[move.i].position.y = move.y;
 
-app.get("/", function(req, res) {
-  res.send(magnets);
-});
-
-app.get("/:magnetId", function(req, res) {
-  if (magnets[req.params.magnetId]) {
-    res.send(magnets[req.params.magnetId]);
-  }
-  else {
-    res.status(404).end("not found");
-  }
-});
-
-app.patch("/:magnetId", function(req, res) {
-  var magnet = magnets[req.params.magnetId];
-  if (!magnet) {
-    res.status(404).end("not found");
-    return;
-  }
-
-  if (isNaN(req.body.rotation) || isNaN(req.body.x) || isNaN(req.body.y)) {
-    res.status(403).end("gotcha");
-  }
-
-  magnet.x = parseFloat(req.body.x);
-  magnet.y = parseFloat(req.body.y);
-  magnet.rotation = parseFloat(req.body.rotation);
-
-  res.send(magnet);
-});
-
-http.listen(port, function(){
-  console.log("listening on " + port);
+        // Broadcast to everyone else.
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(move));
+            }
+        });
+    });
 });
